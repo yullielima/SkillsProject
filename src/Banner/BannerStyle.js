08e923e0af6c2c9fee66187fb9ed4b7bf2cce814
yullielimagit config --global user.email yullielimas@gmail.com <yullielimas@gmail.com>
2022-04-14 14:48:47 -0400

    create media queries

--- src/Banner/BannerStyle.js
+++ src/Banner/BannerStyle.js
@@ -26,6 +26,14 @@ export const BannerContent = styled.div`
   height: 100%;
   padding: 0 75px 0 75px;
   position: relative;
+
+  @media (max-width: 768px) {
+    padding: 0 62px 0 62px;
+  }
+
+  @media (max-width: 414px) {
+    padding: 0 21px 0 21px;
+  }
 `;
 
 export const BannerTitle = styled.h1`
@@ -33,12 +41,30 @@ export const BannerTitle = styled.h1`
   font-weight: 700px;
   color: white;
   margin: 274px 0 28px 0;
+
+  @media (max-width: 768px) {
+    font-size: 66px;
+    margin-top: 186px;
+  }
+
+  @media (max-width: 414px) {
+    font-size: 42px;
+    margin: 191px 0 10px 0;
+  }
 `;
 
 export const BannerSubtitle = styled.h2`
   font-weight: 400;
   font-size: 32px;
   color: white;
+
+  @media (max-width: 768px) {
+    font-size: 30px;
+  }
+
+  @media (max-width: 414px) {
+    font-size: 24px;
+  }
 `;
 
 export const BannerSubText = styled.p`
@@ -46,6 +72,14 @@ export const BannerSubText = styled.p`
   font-size: 22px;
   color: white;
   margin-bottom: 4px;
+
+  @media (max-width: 768px) {
+    font-size: 20px;
+  }
+
+  @media (max-width: 414px) {
+    font-size: 15px;
+  }
 `;
 
 export const BannerButton = styled.button`
@@ -69,10 +103,25 @@ export const BannerButton = styled.button`
       color: #59adb2;
     }
   }
+
+  @media (max-width: 768px) {
+    bottom: 23px;
+  }
+
+  @media (max-width: 414px) {
+    bottom: 12px;
+    width: 35px;
+    height: 35px;
+  }
 `;
 
 export const BannerButtonChevronDown = styled(Img)`
   width: 24px;
   height: 28px;
   color: #f2f1f1;
+
+  @media (max-width: 414px) {
+    width: 16.47px;
+    height: 19.22px;
+  }
 `;
